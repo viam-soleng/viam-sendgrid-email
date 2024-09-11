a06d861914b551bd4654761d94437c8d39f2cece
Matt Vella <mcvella@gmail.com>
2024-09-11 16:16:13 -0400

    initial commit

---
+++ src/sendgridEmail.py
@@ -0,0 +1,106 @@
+from typing import ClassVar, Mapping, Sequence, Any, Dict, Optional, Tuple, Final, List, cast
+from typing_extensions import Self
+from typing import Final
+
+from viam.resource.types import RESOURCE_NAMESPACE_RDK, RESOURCE_TYPE_SERVICE, Subtype
+
+
+
+
+
+from viam.module.types import Reconfigurable
+from viam.proto.app.robot import ComponentConfig
+from viam.proto.common import ResourceName, Vector3
+from viam.resource.base import ResourceBase
+from viam.resource.types import Model, ModelFamily
+
+from viam.services.generic import Generic
+from viam.logging import getLogger
+from viam.utils import ValueTypes, struct_to_dict
+
+import time
+import asyncio
+import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
+LOGGER = getLogger(__name__)
+
+class preset():
+    subject: str
+    body: str
+
+class sendgridEmail(Generic, Reconfigurable):
+
+    MODEL: ClassVar[Model] = Model(ModelFamily("mcvella", "messaging"), "sendgrid-email")
+    email_client: SendGridAPIClient
+    from_email: str
+    preset_messages: dict
+    enforce_preset: bool
+
+    # Constructor
+    @classmethod
+    def new(cls, config: ComponentConfig, dependencies: Mapping[ResourceName, ResourceBase]) -> Self:
+        my_class = cls(config.name)
+        my_class.reconfigure(config, dependencies)
+        return my_class
+
+    # Validates JSON Configuration
+    @classmethod
+    def validate(cls, config: ComponentConfig):
+        api_key = config.attributes.fields["api_key"].string_value
+        if api_key == "":
+            raise Exception("An api_key must be defined")
+        
+        enforce_preset = config.attributes.fields["enforce_preset"].bool_value
+        if enforce_preset == True:
+            attributes = struct_to_dict(config.attributes)
+            preset_messages = attributes.get("preset_messages")
+            if preset_messages is None:
+                raise Exception("preset_messages must be defined when enforce_preset is set to true")
+        return
+
+    # Handles attribute reconfiguration
+    def reconfigure(self, config: ComponentConfig, dependencies: Mapping[ResourceName, ResourceBase]):
+        attributes = struct_to_dict(config.attributes)
+        preset_messages = attributes.get("preset_messages") or {}
+        for p in preset_messages:
+            self.preset_messages['p'] = preset(**preset_messages[p])
+
+        self.enforce_preset = config.attributes.fields["from_email"].bool_value or False
+        self.from_email = config.attributes.fields["from_email"].string_value or ""
+
+        api_key = config.attributes.fields["api_key"].string_value
+        self.email_client = SendGridAPIClient(api_key)
+        return
+    
+    async def do_command(
+                self,
+                command: Mapping[str, ValueTypes],
+                *,
+                timeout: Optional[float] = None,
+                **kwargs
+            ) -> Mapping[str, ValueTypes]:
+        result = {}
+
+        if 'command' in command:
+            if command['command'] == 'send':
+                message_args = {}
+                if self.enforce_preset and not "preset" in command:
+                    return "preset message must be specified"
+
+                if 'preset' in command:
+                    message_args['html_content'] = self.preset_messages[command['preset']].body
+                    message_args['subject'] = self.preset_messages[command['preset']].subject
+                else:
+                    message_args['html_content'] = command['body'] or ""
+                    message_args['subject'] = command['subject'] or ""
+                
+                if 'to' in command:
+                    message_args['to_emails'] = command['to']
+                else:
+                    return "'to' must be defined"
+                
+                message_args['from_email'] = command['from'] or self.from_email
+                response = self.email_client.send(Mail(**message_args))
+                return {"status_code": response.status_code}
